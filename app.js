744bb8391c17e3059f63f9f8c54d7a6a4d1a3c0d
davkhech <davkhech@gmail.com>
2016-06-26 15:26:09 +0400

    websocket integrated

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var WebSocketServer = require('ws').Server;
+var server = require('http').createServer();
+var wss = new WebSocketServer({
+    server
+});
+var url = require('url');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,12 +22,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 8080);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,67 +38,39 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+// error handler
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
-      message: err.message,
-      error: err
+        message: err.message,
+        error: err
     });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
-app.listen(8080, () => {
-  console.log('Server listening on port 8080!');
+wss.on('connection', (ws) => {
+    console.log('Socket connection established');
+
+    ws.on('message', (message) => {
+        console.log('received: %s', message);
+        ws.send('back to you -> '+ message);
+    });
+
+    ws.send('something');
+});
+
+server.on('request', app);
+
+server.listen(app.get('port'), function() {
+    console.log('Listening on ' + server.address().port)
 });
 
 
-// var server = require('http').createServer()
-//   , url = require('url')
-//   , WebSocketServer = require('ws').Server
-//   , wss = new WebSocketServer({ server: server })
-//   , express = require('express')
-//   , app = express()
-//   , port = 4080;
-
-// app.use(function (req, res) {
-//   res.send({ msg: "hello" });
+// app.listen(app.get('port'), () => {
+//   console.log('Server listening on port 8080!');
 // });
-
-// wss.on('connection', function connection(ws) {
-//   var location = url.parse(ws.upgradeReq.url, true);
-//   // you might use location.query.access_token to authenticate or share sessions
-//   // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//   });
-
-//   ws.send('something');
-// });
-
-// server.on('request', app);
-// server.listen(port, function () { console.log('Listening on ' + server.address().port) });
-
-
-module.exports = app;
